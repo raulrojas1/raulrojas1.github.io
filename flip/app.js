f39028861b84d62df9516dfe6718777ce764e3dd
Raul <rojasraul99@outlook.es>
2022-02-01 18:56:37 -0500

    Fuegos artificiales al ganar

--- flip/app.js
+++ flip/app.js
@@ -130,6 +130,30 @@ function createBoard(){
 	}
 }
 
+function WinAnimation(){
+	//Fireworks confetti
+	//win animation
+		var duration = 15 * 1000;
+		var animationEnd = Date.now() + duration;
+		var defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+		function randomInRange(min, max) {
+		  return Math.random() * (max - min) + min;
+		}
+		var interval = setInterval(function() {
+		var timeLeft = animationEnd - Date.now();
+
+		  if (timeLeft <= 0) {
+		    return clearInterval(interval);
+		  }
+
+		  var particleCount = 300 * (timeLeft / duration);
+		  // since particles fall down, start a bit higher than random
+		  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+		  confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+		}, 250)
+}
+
 
  
 //check for matches
@@ -138,7 +162,7 @@ function checkForMatch(){
 	const optionOneId = cardsChosenId[0]
 	const optionTwoId = cardsChosenId[1]
 
-	if (cardsChosen[0] === cardsChosen[1] /*& optionOneId != optionTwoId*/) {
+	if (cardsChosen[0] === cardsChosen[1] ) {
 		
 		//cards[optionOneId].setAttribute('src','images/white.png')
 		cards[optionOneId].style.opacity = '0'
@@ -153,26 +177,32 @@ function checkForMatch(){
 		cards[optionOneId].style.userSelect = "none"
 
 		cardsWon.push(cardsChosen)
-		confetti({
-		  particleCount: 1000,
-		  startVelocity: 30,
-		  spread: 360,
-		  origin: {
-		    x: Math.random(),
-		    // since they fall down, start a bit higher than random
-		    y: Math.random() - 0.2
-		  }
-		});
+		if (cardsWon.length>=10) {
+			resultDisplay.textContent = "10 Ganasteee!!!"
+			WinAnimation()
+		} else{
+		//Normal Confetti
+		resultDisplay.textContent = cardsWon.length
+			confetti({
+			  particleCount: 1000,
+			  startVelocity: 30,
+			  spread: 360,
+			  origin: {
+			    x: Math.random(),
+			    // since they fall down, start a bit higher than random
+			    y: Math.random() - 0.2
+			  }
+			})
+		}
+
+		
 	}else {
 		cards[optionOneId].setAttribute('src','images/blank.png')
 		cards[optionTwoId].setAttribute('src','images/blank.png')
-		//alert('Mal')
-		//alert(cardsChosen[0]+' '+cardsChosen[1])
 	}
 
 	cardsChosen = []
 	cardsChosenId = []
-	resultDisplay.textContent = cardsWon.length
 
 }
 
